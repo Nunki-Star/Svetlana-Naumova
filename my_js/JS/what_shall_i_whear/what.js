aacb13d1aeb24287d7d6cbb159eaa1b9a1aa07c9
Svetlana-Naumova <tiggernunki@mail.ru>
2023-04-08 17:04:08 +0300

    file js renaimed, 2 js files added

---
+++ my_js/JS/what_shall_i_whear/what.js
@@ -0,0 +1,13 @@
+function whatShallIWhear(temp) {
+    if (temp < 60) {
+        console.log("whear a jacket");
+    } else if (temp < 70) {
+        console.log("wear a sweater");
+    } else {
+        console.log("wear a t-shirt");
+    }
+}
+
+whatShallIWhear(50);
+whatShallIWhear(60);
+whatShallIWhear(80);
