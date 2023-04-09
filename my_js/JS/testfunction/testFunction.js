6dc715b1e6e521a9b96211576f522876c250ffcf
Svetlana-Naumova <tiggernunki@mail.ru>
2023-04-09 20:20:04 +0300

    js file testFunction created

---
+++ my_js/JS/testfunction/testFunction.js
@@ -0,0 +1,14 @@
+function testFunction(){
+    const x = 5;
+    const y = 10;
+    console.log(x + y)
+}
+
+function printHello(){
+    console.log("hello world!")
+}
+
+testFunction()
+testFunction()
+printHello()
+printHello()
