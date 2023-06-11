3b82ac0a91e8c74ea2468fc2450ff758aebba5d3
Svetlana-Naumova <tiggernunki@mail.ru>
2023-06-11 19:25:01 +0300

    js second corrected

--- javascript/a_second/second.js
+++ javascript/a_second/second.js
@@ -7,14 +7,29 @@ var infoName = lastName + ' ' + yourName + ' ' + fathersName
 var numberYears = Number(yourAge)
 var ageDays = numberYears * 365
 var infiveYears = numberYears + 5
+/*if (maleGender == true){
+    var yourGender = "male"
+    if (yourAge >= 63){
+        var retired = "You are retired"
+    }
+    else{
+        var retired = "You are not retired"
+    }
+}
+else{
+    var yourGender = "female"
+    if (yourAge >= 58){
+        var retired = "You are retired"
+    }
+    else{
+        var retired = "You are not retired"
+    }
+}*/
 
-var retired = (maleGender == true) ? (yourAge >= 63) ? "retired" : "not retired":
-    (maleGender == false) ? (yourAge >= 58) ? "retired" : "not retired"
-    
-
+var retired = (maleGender==true) ? (yourGender = "male") ? (yourAge>=63) ? "You are retired" : "You are not retired": (yourGender ="female") ? (yourAge>=58) ? "You are retired" : "You are not retired"
 alert("Your name is : " + infoName + '\n' +
       "Your age is : " + yourAge + '\n' +
       "Your age is equal " + ageDays + " days" + '\n' +
       "In five years youll be " + infiveYears + '\n' +
-      "Your gender is " + maleGender + '\n' +
-       retired)
+      "Your gender is " + yourGender + '\n'+
+      retired )
