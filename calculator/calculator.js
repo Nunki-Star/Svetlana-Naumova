a925fb1312fefe08de192cb28f06ac59159e52b1
Svetlana-Naumova <tiggernunki@mail.ru>
2023-06-15 21:49:26 +0300

    calculator adjusted

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,52 @@
-function calculator(a, b, operation){
-    switch(operation){
-        case "+":
+/*function calculator(a, b, operation){
+    switch (operation){
+        case "+" :
             console.log(a+b);
-            break
-    }
-    switch(operation){
+            break;
         case "-":
+            console.log(a-b);
+            break;
+        case "*":
+            console.log(a*b);
+            break;
+        case "/":
             console.log(a-b);
             break
     }
+}
+calculator(6, 1, "-")*/
+
+/*function calc(a, b, operation){
     switch(operation){
+        case "+":
+            return(a+b);
+            
+        case "-":
+            return(a-b);
+            
         case "*":
-            console.log(a*b);
-            break
-    }
-    switch(operation){
+            return(a*b);
+           
         case "/":
-            console.log(a/b);
-            break
+            return(a/b)
+
     }
 }
 
-calculator(9, 2, "*")
+var item1=calc(7, 6, "-");
+var item2=calc(9, 9, "/")
+
+console.log(item1+item2)*/
+
+var x = null, y = null
+
+function getNumber(num){
+    
+        if(x === null){
+            x=num
+        }
+        else{
+            y=num
+        }
+    }
 
---
+++ rainbow/rainbow.js
@@ -0,0 +1,14 @@
+var rainbowInt = parseInt(prompt("please enter the number of the colour"))
+var colourRain;
+switch (rainbowInt){
+    case 1 :
+        colourRain="red";
+        break;
+    case 2 :
+        colourRain="blue";
+        break;
+    case 3 :
+        colourRain="green";
+        break;
+}
+console.log(colourRain)
