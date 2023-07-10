e9d480ec244a7ddc8b38fd35922143d1558bb2e4
Svetlana-Naumova <tiggernunki@mail.ru>
2023-07-10 12:45:48 +0300

    js practice added

---
+++ udemy/folder1/first.js
@@ -0,0 +1,83 @@
+//let number = 5;
+//const leftBoderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//const obj = {
+    //name: "Svetlana",
+    //age: 36,
+    //isMarried: false
+//
+//};
+
+//console.log(obj["name"])
+
+
+
+//let arr = [1, 2, 3];
+
+//console.log(arr[1]);
+
+
+//const obj = {a: 1, b: 2};
+
+//console.log(obg)
+
+
+ //const test = []
+
+ //test[0] = prompt("how are you", "")
+ //test[1] = prompt("how is it going?", "")
+ //test[3] = prompt("are you ok?", "")
+
+ //document.write(test)
+
+ //var scoopsOfIcecream = 5;
+// while (scoopsOfIcecream > 0) {
+    //document.write("Another scoop!<br>");
+    //scoopsOfIcecream = scoopsOfIcecream - 1;
+ //}
+//document.write("Life without icecream isnt the same!")
+
+/*const obj ={
+    name: "Svetlana",
+    age: 36,
+}
+
+console.log(obj["name"])*/
+
+/*const arr = [1, 2, 3]
+
+console.log(arr[0])
+
+arr[9]=4
+console.log(arr)
+
+
+
+const obj = {
+    one:1,
+    two:2,
+    three:3
+}
+console.log(obj)
+console.log(obj.two)
+
+
+obj.four = 4
+
+console.log(obj)
+
+const answerPrompt = +prompt("How old are you", "i have 18")
+
+console.log(answerPrompt + 5)
+
+
+const confirmAnswer = confirm("Are you 18?")
+
+console.log(confirmAnswer)*/
+
+const someVariable = "this is what we add"
+
+console.log(`this is how to use interpplation, we use dollar and curly braskets ${someVariable}`)
